616fbab192e0e2e72b7bdacf191ec9147c1acc32
majid-amini <majidamini_s@yahoo.com>
2022-11-09 15:15:02 +0330

    title item's bugs fixed

--- src/components/DateAndTitle.js
+++ src/components/DateAndTitle.js
@@ -3,14 +3,14 @@ function DateAndTitle(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <div className="user__info__left">
+    
       <div className="date__info">
         <span className="date__info__year">{year}</span>
         <span className="date__info__month">{month}</span>
         <span className="date__info__day">{day}</span>
       </div>
-      <div className="title">{props.title}</div>
-    </div>
+      
+    
   );
 }
 export default DateAndTitle;
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,7 +2,10 @@ import DateAndTitle from "./DateAndTitle";
 function UserInfo(props) {
   return (
     <div className="user__info">
+      <div className="user__info__left"> 
       <DateAndTitle date={props.date} />
+      <div className="title">{props.title}</div>
+      </div>
       <div className="user__info__right">
         <span>${props.amount}</span>
       </div>
