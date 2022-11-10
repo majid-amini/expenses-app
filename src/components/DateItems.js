e68be70c6c5b3fa84ad338efe6b69c44178a1b1d
majid-amini <majidamini_s@yahoo.com>
2022-11-10 16:02:03 +0330

    some styles changed

---
+++ src/components/DateItems.js
@@ -0,0 +1,13 @@
+function DateItems(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  return (
+    <div className="date__info">
+      <span className="date__info__year">{year}</span>
+      <span className="date__info__month">{month}</span>
+      <span className="date__info__day">{day}</span>
+    </div>
+  );
+}
+export default DateItems;
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,10 +1,10 @@
-import DateAndTitle from "./DateAndTitle";
+import DateItems from "./DateItems";
 function UserInfo(props) {
   return (
     <div className="user__info">
-      <div className="user__info__left"> 
-      <DateAndTitle date={props.date} />
-      <div className="title">{props.title}</div>
+      <div className="user__info__left">
+        <DateItems date={props.date} />
+        <div className="title">{props.title}</div>
       </div>
       <div className="user__info__right">
         <span>${props.amount}</span>
