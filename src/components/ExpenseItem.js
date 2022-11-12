adba557da9b29e031db47b70a4ae90b5a08fceec
majid-amini <majidamini_s@yahoo.com>
2022-11-12 12:25:47 +0330

    form element and inputs added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,30 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  return <div className="expense_container">
-            <button>Add New Expense</button>
-        </div>;
+  const submitForm = (e) => {
+    const title = document.querySelector('.title_text');
+    e.preventDefault();
+    console.log(title.value);
+  };
+  return (
+    <div className="expense_container">
+      <form onSubmit={submitForm}>
+        <div>
+          <label>Title</label>
+          <input className="title_text" type="text" />
+        </div>
+        <div>
+          <label>Amount</label>
+          <input id="amount" min={0.1} max={10} type="number" />
+        </div>
+        <div>
+          <label>Date</label>
+          <input id="date" type="date" />
+        </div>
+        <button type="submit">Add New Expense</button>
+      </form>
+    </div>
+  );
 }
 
 export default ExpenseItem;
